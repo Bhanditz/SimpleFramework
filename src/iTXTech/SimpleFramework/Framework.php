de5f1368bae30bb4e6d862bab093e89e135f7ed1
PeratX <1215714524@qq.com>
2018-12-09 16:46:19 +0800

    Framework: move to 2.2.0 "Kassandra" (API 7)

--- src/iTXTech/SimpleFramework/Framework.php
+++ src/iTXTech/SimpleFramework/Framework.php
@@ -36,9 +36,9 @@ use iTXTech\SimpleFramework\Util\Util;
 
 class Framework implements OnCompletionListener{
 	public const PROG_NAME = "SimpleFramework";
-	public const PROG_VERSION = "2.1.0-beta.1";
-	public const API_LEVEL = 6;
-	public const CODENAME = "Navi";
+	public const PROG_VERSION = "2.2.0-SNAPSHOT";
+	public const API_LEVEL = 7;
+	public const CODENAME = "Kassandra";
 
 	/** @var Framework */
 	private static $instance = null;
